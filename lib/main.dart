b80269ae2f97806416b35191937cceeb06ea4aa6
Lucas Lamin <lucaslamin15@gmail.com>
2021-06-21 23:57:31 -0300

    Dia 2

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:payflow/modules/login/login_page.dart';
+import 'package:payflow/modules/splash/splash_page.dart';
+import 'package:payflow/shared/themes/app_colors.dart';
+
+
+void main() {
+  runApp(AppWidget());
+  }
+  
+ 
+class AppWidget extends StatelessWidget {  
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Pay Flow',
+      theme: ThemeData(  
+        primaryColor: AppColors.primary,       
+      ),
+      home: LoginPage(),
+    );
+  }
+}
