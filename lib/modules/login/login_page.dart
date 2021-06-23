9c408c572374006f9c6a944cd91d944bc40b9224
Lucas Lamin <lucaslamin15@gmail.com>
2021-06-23 15:03:41 -0300

    Firebase install

--- lib/modules/login/login_page.dart
+++ lib/modules/login/login_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:payflow/modules/login/login_controller.dart';
 import 'package:payflow/shared/themes/app_colors.dart';
 import 'package:payflow/shared/themes/app_images.dart';
 import 'package:payflow/shared/themes/app_text_styles.dart';
@@ -13,6 +15,7 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final controller = LoginController();
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
@@ -56,13 +59,15 @@ class _LoginPageState extends State<LoginPage> {
                   ),
                   Padding(
                     padding: const EdgeInsets.only(left: 40, right: 40, top: 40),
-                    child: SocialLoginButton(onTap: (){
-                        print("clicou");
-                    },),
-                  )  
+                    child: SocialLoginButton(
+                      onTap: () {
+                        controller.googleSignIn(context);
+                      }
+                    ),
+                  )
                 ],
               ),
-            ), 
+            ),
           ],
         ),
       ),
